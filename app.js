73bd21479a52093cd07c844fa4d3f9e86d7facec
RogerSelwyn <RogerSelwyn@users.noreply.github.com>
2018-10-24 17:44:01 +0100

    added routing by id for readmore

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('./config/config.js')
 
 const express = require('express');
 const hbs = require('hbs');
+// const helpers = require('handlebars-helpers')(['array']);
 
 const r = require('./routes/route')
 
@@ -13,10 +14,11 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
+// hbs.registerHelper("filter", helpers.filter);
 
 app.get('/', r.routes.home);
 
-app.get('/readmore', r.routes.readmore);
+app.get('/readmore/:id', r.routes.readmore);
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
